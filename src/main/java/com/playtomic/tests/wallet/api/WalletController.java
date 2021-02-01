4a85270630700c2f77b36efe4028b5cb94527e6b
andreykolchev <andreykolchev@gmail.com>
2021-02-01 16:15:41 +0200

    added some logging

--- src/main/java/com/playtomic/tests/wallet/api/WalletController.java
+++ src/main/java/com/playtomic/tests/wallet/api/WalletController.java
@@ -7,10 +7,12 @@ import com.playtomic.tests.wallet.domain.dto.WithdrawDto;
 import com.playtomic.tests.wallet.ex.ErrorException;
 import com.playtomic.tests.wallet.ex.ErrorType;
 import com.playtomic.tests.wallet.service.WalletService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/wallets")
 public class WalletController {
@@ -23,17 +25,20 @@ public class WalletController {
 
     @GetMapping("/{id}")
     public ResponseEntity<WalletDto> getByID(@PathVariable("id") String id) {
+        log.info("Wallet request with id: {}", id);
         return new ResponseEntity<>(walletService.getWalletById(id), HttpStatus.OK);
     }
 
     @PostMapping("/charge")
     public ResponseEntity<Void> charge(@RequestBody ChargeDto dto) {
+        log.info("Charge wallet request with id: {}", dto.getWalletId());
         walletService.charge(dto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/withdraw")
     public ResponseEntity<Void> withdraw(@RequestBody WithdrawDto dto) {
+        log.info("Withdraw wallet request with id: {}", dto.getWalletId());
         walletService.withdraw(dto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
